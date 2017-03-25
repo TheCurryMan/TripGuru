92f223fd5c0e7e38c422fe0c5e5288f85d1c5b88
Avinash Jain <avinashkalpitjain@gmail.com>
2017-03-25 14:06:57 -0700

    Added basic flask app

---
+++ TripGuruServer/README.md
@@ -0,0 +1,28 @@
+Flask Heroku Sample
+====================
+
+A simple Python Flask example application that's ready to run on Heroku.
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+## Development Setup
+
+* `virtualenv venv`
+
+* `source venv/bin/activate`
+
+* `pip install -r requirements.txt`
+
+* `python app.py`
+
+## Deploy
+
+* `heroku create`
+
+* `heroku addons:create heroku-postgresql:hobby-dev`
+
+* `git push heroku master`
+
+## Contributors
+
+* [Yefim](https://twitter.com/yefim)
---
+++ TripGuruServer/app.py
@@ -0,0 +1,39 @@
+import os
+
+from flask import Flask, render_template, request, redirect, url_for
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:////tmp/flask_app.db')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(100))
+  email = db.Column(db.String(100))
+
+  def __init__(self, name, email):
+    self.name = name
+    self.email = email
+
+
+@app.route('/', methods=['GET'])
+def index():
+  return render_template('index.html', users=User.query.all())
+
+
+@app.route('/user', methods=['POST'])
+def user():
+  u = User(request.form['name'], request.form['email'])
+  db.session.add(u)
+  db.session.commit()
+  return redirect(url_for('index'))
+
+if __name__ == '__main__':
+  db.create_all()
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port, debug=True)
