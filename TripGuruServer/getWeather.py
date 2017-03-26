7e2b3465bf5053ebde5f1c14e11e53315cf02c03
Avinash Jain <avinashkalpitjain@gmail.com>
2017-03-26 06:34:25 -0700

    Updated weather function

--- TripGuruServer/getWeather.py
+++ TripGuruServer/getWeather.py
@@ -2,13 +2,16 @@ import requests
 import ast
 API_key = "1763e5ce6e054083800110513172603"
 
-twelve_hour = ""
+
 
 def get_hourly_weather(city, state):
+    twelve_hour = ""
     url = "https://api.apixu.com/v1/forecast.json?key=" + API_key + "&q=" + city
     r = requests.get(url)
     res = r.text
     rev = ast.literal_eval(res)
     for item in rev['forecast']['forecastday'][0]['hour']:
-        twelve_hour = twelve_hour + item['will_it_rain'] + ","
+        twelve_hour = twelve_hour + str(item['will_it_rain']) + ","
     return twelve_hour[:-1]
+
+print(get_hourly_weather("San Francisco", "asd"))
