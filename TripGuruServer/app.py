d0bf204943016d730b5d5ece12cc45727f69ad5b
Avinash Jain <avinashkalpitjain@gmail.com>
2017-03-26 03:50:33 -0700

    Updating server with reviews

--- TripGuruServer/app.py
+++ TripGuruServer/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import json
 from getAttractionsFromName import getAttractions
+from getReviews import get_revs, calculate_review_num
 
 app = Flask(__name__)
 
@@ -19,4 +20,11 @@ def attractions():
 def sort():
 
   return getAttractions(request.args.get('data'))
+
+@app.route('/reviews', methods=['GET', 'POST'])
+def reviews():
+
+  allReviews = get_revs(request.args.get('attraction'), request.args.get('city'))
+  score = calculate_review_num(allReviews)
+  return allReviews + score
 
---
+++ TripGuruServer/getReviews.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+import ast
+
+def get_revs(attraction, city):
+    attraction = convertSpaces(attraction)
+    city = convertSpaces(city)
+    url = "https://www.yelp.com/biz/" + attraction + "-" + city + "?osq=tourist+attractions"
+    r = requests.get(url)
+
+    data = r.text
+    soup = BeautifulSoup(data, "html.parser")
+
+    reviews = ""
+    for item in soup.findAll("p", {"class": "quote"}):
+        reviews += (item.text[13:-14].encode('ascii', errors='ignore')) + ","
+    return reviews
+
+def convertSpaces(name):
+    return (name.replace(" ", "-" ))
+
+def calculate_review_num(reviews):
+    sum = 0
+    for item in reviews:
+        r = requests.post("http://text-processing.com/api/sentiment/", data={'text': item})
+        revs = r.text
+        rev = ast.literal_eval(revs)
+        label = rev['label']
+        perc = rev['probability'][label]
+        sum = (sum + perc)
+    return sum/len(reviews) * 10
